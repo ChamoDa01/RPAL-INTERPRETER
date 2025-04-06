c3023cdb575f2ddb5829e9cb59ef0f30d7770cde
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-04-06 12:22:36 +0530

    Parser updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
 import lexicalanalyzer.LexicalAnalyzer;
+import lexicalanalyzer.Token;
+import lexicalanalyzer.TokenType;
+import parser.Node;
+import parser.NodeType;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer("C:\\Users\\madhu\\Documents\\RPAL-INTERPRETER\\src\\test\\resources\\Q1.txt");
-        System.out.println(lexicalAnalyzer.tokenize());
+        List<Token> tokens = lexicalAnalyzer.tokenize();
+        Iterator<Token> tokenList = tokens.iterator();
+        Token token = tokenList.next();
+        while (token.getTokenType().equals(TokenType.KEYWORD) && token.getTokenValue().equals("let")) {
+            System.out.println(token);
+            tokenList.remove();
+            token = tokenList.next();
+            if (token.getTokenType().equals(TokenType.KEYWORD) && token.getTokenValue().equals("in")){
+                System.out.println(token);
+                tokenList.remove();
+            }else{
+                System.out.println("Parse error at E");
+            }
+
+            System.out.println(token);
+        }
     }
 }
